7642cadf0f05c9e689385076ec27715819eda1b7
Felipems999 <felipems97@hotmail.com>
2023-01-22 22:31:44 -0300

    Implementando algoritmo na classe r2amedia

--- dash_client.py
+++ dash_client.py
@@ -34,7 +34,8 @@ class DashClient:
         self.player = Player(0)
 
         # automatic loading class by the name
-        r2a_class = getattr(importlib.import_module('r2a.' + r2a_algorithm.lower()), r2a_algorithm)
+        r2a_class = getattr(importlib.import_module(
+            'r2a.' + r2a_algorithm.lower()), r2a_algorithm)
         self.r2a = r2a_class(1)
 
         self.connection_handler = ConnectionHandler(2)
@@ -42,7 +43,6 @@ class DashClient:
         self.modules.append(self.player)
         self.modules.append(self.r2a)
         self.modules.append(self.connection_handler)
-
 
     def run_application(self):
         self.modules_initialization()
@@ -53,17 +53,15 @@ class DashClient:
 
         self.modules_finalization()
 
-
     def handle_scheduler_event(self, event):
 
-        #checking if the event is inside of the modules position range limits
+        # checking if the event is inside of the modules position range limits
         if event.get_dst() < 0 or event.get_dst() >= len(self.modules):
-            print(f'It is no possible to route a {event.get_msg()} message from {event.get_src()} to {event.get_dst()} module list position.')
+            print(
+                f'It is no possible to route a {event.get_msg()} message from {event.get_src()} to {event.get_dst()} module list position.')
             exit(0)
 
         self.modules[event.get_dst()].handle_message(event.get_msg())
-
-
 
     def modules_initialization(self):
         print('Initialization modules phase.')
---
+++ r2a/r2amedia.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Marcos F. Caetano (mfcaetano@unb.br) 03/11/2020
+
+@description: PyDash Project
+
+Abstract Class for R2A (Rate Adaptation Algorithms) implementations
+
+It is necessary to implement all the @abstractmethod methods to generate a new R2A Algorithm implementation
+
+"""
+
+from r2a.ir2a import IR2A
+from player.parser import *
+import time
+
+
+class R2AMedia(IR2A):
+
+    def __init__(self, id):
+        IR2A.__init__(self, id)
+        self.parsed_mpd = ''
+        self.qi = []
+        self.tput = 0
+        self.time_req = 0
+        self.index = int(10)
+
+    def handle_xml_request(self, msg):
+
+        self.time_req = time.time()
+
+        self.send_down(msg)
+
+    def handle_xml_response(self, msg):
+        # getting qi list
+        self.parsed_mpd = parse_mpd(msg.get_payload())
+        self.qi = self.parsed_mpd.get_qi()
+
+        var_time = time.time() - self.time_req
+
+        self.time_req = var_time
+
+        self.tput = msg.get_bit_length()/var_time
+
+        print(f'\nThroughtput : {self.tput}')
+
+        self.send_up(msg)
+
+    def handle_segment_size_request(self, msg):
+
+        self.index = self.busca_bin(
+            0, int(self.qi.__len__()/2), self.qi.__len__() - 1)
+
+        msg.add_quality_id(self.qi[self.index])
+        print(f"\nqi[{self.qi[self.index]}]\n")
+
+        self.time_req = time.time()
+
+        self.send_down(msg)
+
+    def handle_segment_size_response(self, msg):
+
+        var_time = time.time() - self.time_req
+
+        self.tput = msg.get_bit_length()/var_time
+        print(f'\nThroughtput: {self.tput}\n')
+
+        self.send_up(msg)
+
+    def busca_bin(self, low, mid, hi):
+
+        if (self.tput >= self.qi[hi]):
+            return hi
+        elif (self.tput <= self.qi[low]):
+            return low
+
+        while (low <= hi):
+            if (self.tput == self.qi[mid]
+                    or (self.tput > self.qi[mid] and self.tput < self.qi[mid + 1])):
+                return int(mid)
+            elif (self.tput < self.qi[mid] and self.tput >= self.qi[mid - 1]):
+                return int(mid - 1)
+
+            if (self.tput < self.qi[mid - 1]):
+                hi = mid
+                mid = int(hi / 2)
+            elif (self.tput > self.qi[mid + 1]):
+                low = mid + 1
+                mid = int(((low + hi) / 2))
+
+    def initialize(self):
+        print('Iniciando\n')
+        pass
+
+    def finalization(self):
+        print('Finalizando')
+        pass
